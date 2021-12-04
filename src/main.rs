a241caf3027b3aafa48a0b932a35fe36b6336195
Zsolt Endreffy <zsolt.endreffy@commsignia.com>
2021-12-04 19:00:13 +0100

    first day second task

--- src/main.rs
+++ src/main.rs
@@ -6,13 +6,15 @@ fn main() {
         .expect("Something went wrong reading the file");
 
     let inputs: Vec<i32> = input_string.strip_suffix("\n").unwrap().split("\n").map(|v| v.parse::<i32>().unwrap()).collect();
-    let mut increases = 0;
+    let mut increases = -1;
+    let mut previous_sum = 0;
 
     for (index, input) in inputs.iter().enumerate() {
-        if index != 0 {
-            if input > &inputs[index-1] {
+        if index > 1 {
+            if (*input + inputs[index - 1] + inputs[index - 2]) > previous_sum {
                 increases = increases + 1;
             }
+            previous_sum = *input + inputs[index - 1] + inputs[index -2];
         }
     }
     println!("{}", increases);
