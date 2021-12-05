4ff3fef6795f8b711b4f645aa5cd32fb842cfe14
Zsolt Endreffy <zsolt.endreffy@commsignia.com>
2021-12-05 20:28:27 +0100

    second day second task
    Also removed the backward motion,
    because it does not exist.

--- src/main.rs
+++ src/main.rs
@@ -46,22 +46,35 @@ fn solve_day_1(task: i8, raw_inputs: Vec<String>) -> i32 {
     }
 }
 
-fn solve_day_2(raw_inputs: Vec<String>) -> i32 {
+fn solve_day_2(task: i8, raw_inputs: Vec<String>) -> i32 {
     let mut horizontal = 0;
     let mut vertical = 0;
+    let mut aim = 0;
     for movement in raw_inputs {
         let mut iter = movement.split_whitespace();
         let direction = iter.next();
         let amount = iter.next().unwrap().parse::<i32>().unwrap();
-        match direction {
-            Some("forward") => horizontal += amount,
-            Some("back") => horizontal += amount,
-            Some("up") => vertical -= amount,
-            Some("down") => vertical += amount,
-            _ => panic!("incorrect input")
+        if task == 1 {
+            match direction {
+                Some("forward") => horizontal += amount,
+                Some("up") => vertical -= amount,
+                Some("down") => vertical += amount,
+                _ => panic!("incorrect input")
+            }
+        }
+        else {
+            match direction {
+                Some("forward") => {
+                    horizontal += amount;
+                    vertical = vertical + aim * amount;
+                },
+                Some("up") => aim -= amount,
+                Some("down") => aim += amount,
+                _ => panic!("incorrect input")
+            }
         }
     }
-    return horizontal * vertical
+    return horizontal * vertical;
 }
 
 fn main() {
@@ -72,7 +85,7 @@ fn main() {
     let raw_inputs = read_input_file(&format!("day/{}/input", selector.day));
     match selector.day {
         1 => println!("{}", solve_day_1(selector.task, raw_inputs)),
-        2 => println!("{}", solve_day_2(raw_inputs)),
+        2 => println!("{}", solve_day_2(selector.task, raw_inputs)),
         _ => println!("Not implemented (yet)")
     }
 }
